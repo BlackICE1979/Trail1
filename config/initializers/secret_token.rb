11c82d4a237845ff96df800da901a4341a0eeb6b
Jean-Francois Morneau <jmorneau@ubuntu.(none)>
2010-11-06 03:37:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trail1::Application.config.secret_token = '28721d6a219a2e9e52bf2d3cae4f5c80047b142dc2a166fad38bdc5f82fe502931408c04f1c1207d43e5802fdefabaa477192745dba3ce0830609a2e7f5f8839'
